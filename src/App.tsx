babda413f56d6fed9f088dc69021edf4a813d36d
Lisa Mapp <218020739+Nails8919@users.noreply.github.com>
2026-04-23 21:27:11 -0400

    Added some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
-import HomePage from "./components/HomePage"
+// import HomePage from "./components/HomePage"
 import Navbar from "./components/NavBar"
 import ShowMovies from "./components/ShowMovies"
-import { Routes, Route } from "react-router-dom"
-import NotFound from "./components/NotFound"
+// import { Routes, Route } from "react-router-dom"
+// import NotFound from "./components/NotFound"
+import { useState } from "react"
 
 
 const App = () => {
+  const [contentType, setContentType] =
+    useState<'movies' | 'series' | 'all'>('all')
   return (
     <>
-    <Navbar />
+      <Navbar contentType={contentType} setContentType={setContentType} />
+      <ShowMovies contentType={contentType} />
+    </>
+  )
+}
+{/* <>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/movies" element={<ShowMovies />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
-    </>
-  )
-}
+    </> */}
 
 export default App
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,48 @@
 import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+
+interface NavbarProps {
+  contentType: 'movies' | 'series' | 'all'
+  setContentType: React.Dispatch<
+    React.SetStateAction<'movies' | 'series' | 'all'>
+  >
+}
+
+const Navbar = ({ contentType, setContentType }: NavbarProps) => {
   return (
     <>
+      <div>
+        <Link className="mx-4 underline" to="/">Home</Link>
+      </div>
+
       <div className="flex justify-center p-4 bg-gray-200">
-      <Link className="mx-4 underline" to="/">Home</Link>
-      <Link className="mx-4 underline" to="/movies">Movies</Link>
-      <Link className="mx-4 underline" to="/Tv Shows">TV Shows</Link>
-      <Link className="mx-4 underline" to="/myfave">My Faves</Link>
+        <select
+          className="mb-4 w-9/12 rounded-lg border p-2"
+          value={contentType}
+          onChange={(e) =>
+            setContentType(e.target.value as 'movies' | 'series' | 'all')
+          }
+        >
+          <option value="all">All</option>
+          <option value="movies">Movies</option>
+          <option value="series">Series</option>
+        </select>
       </div>
     </>
   )
 }
+
+// const Navbar = () => {
+//   return (
+//     <>
+//       <div className="flex justify-center p-4 bg-gray-200">
+//       <Link className="mx-4 underline" to="/">Home</Link>
+//       <Link className="mx-4 underline" to="/movies">Movies</Link>
+//       <Link className="mx-4 underline" to="/Tv Shows">TV Shows</Link>
+//       <Link className="mx-4 underline" to="/myfave">My Faves</Link>
+//       </div>
+//     </>
+//   )
+// }
 
 export default Navbar
